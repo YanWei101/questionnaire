d726c2894154574b39a4ad0ab18e943801007ba3
YanWei101 <zijie101567@gmail.com>
2025-02-11 15:04:36 +0800

    feat:增加删除问卷，新增问卷和发布问卷的页面效果

---
+++ src/component/questionList.tsx
@@ -0,0 +1,59 @@
+'use client'
+import React, {useState} from 'react';
+import QuestionCard from "@/component/questioncard";
+
+export default function QuestionList() {
+
+    const [questionList, setQuestionList] = useState([
+        {id: 'q1', title: '问卷1', isPublished: true},
+        {id: 'q2', title: '问卷2', isPublished: false},
+        {id: 'q3', title: '问卷3', isPublished: true},
+        {id: 'q4', title: '问卷4', isPublished: true},
+    ])
+
+    function deleteQuestion(id: string) {
+        setQuestionList(questionList.filter((q) => {
+            return q.id !== id;
+        }))
+    }
+
+    function publishQuestion(id: string) {
+        setQuestionList(questionList.map((q) => {
+            if (q.id === id) {
+                q.isPublished = true
+            }
+            return q
+        }))
+    }
+        function add() {
+            const r = Math.random().toString().slice(-3)
+            setQuestionList(questionList.concat(
+                {
+                    id: 'q' + r,
+                    title: '问卷' + r,
+                    isPublished: false
+                }
+            ))
+        }
+        return (
+            <div>
+                <h1>问卷列表</h1>
+                {
+                    questionList.map((q) => {
+                        return (
+                            <QuestionCard
+                                key={q.id}
+                                id={q.id}
+                                title={q.title}
+                                isPublished={q.isPublished}
+                                deleteQuestion={deleteQuestion}
+                                publishQuestion={publishQuestion}
+                            />
+                        )
+                    })
+                }
+                <button onClick={add}>添加问卷</button>
+            </div>
+        );
+    }
+
